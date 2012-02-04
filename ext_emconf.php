171d64f3bf58a9a5b96021fc9aa207473e17addb
Alexander Stehlik <alexander.stehlik@googlemail.com>
2012-02-04 08:51:45 +0100

    Initial release
    * repository created
    * ext_emconf.php created

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "formhandler_subscription".
+#
+# Auto generated 09-01-2012 21:01
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'URL shortener',
+	'description' => 'This extensions allows you to cut down long URLs. It basically works like bitly or TinyURL.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Alexander Stehlik',
+	'author_email' => 'alexander.stehlik.deleteme@googlemail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array(
+	),
+);
+
+?>
