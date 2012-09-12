fdac0237ba7034df60b537c6d6f05eb3827b30c7
Alexander Stehlik <alexander.stehlik@googlemail.com>
2012-09-12 15:41:38 +0200

    Released version 0.0.1 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "formhandler_subscription".
+# Extension Manager/Repository config file for ext "tinyurls".
 #
-# Auto generated 09-01-2012 21:01
+# Auto generated 12-09-2012 15:39
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extensions allows you to cut down long URLs. It basically works like bitly or TinyURL.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:14:{s:16:"ext_autoload.php";s:4:"1f47";s:21:"ext_conf_template.txt";s:4:"5953";s:17:"ext_localconf.php";s:4:"39f3";s:14:"ext_tables.php";s:4:"0cca";s:14:"ext_tables.sql";s:4:"bb83";s:30:"Classes/Hooks/EidProcessor.php";s:4:"9a42";s:21:"Classes/Hooks/Tce.php";s:4:"bef5";s:26:"Classes/Hooks/TypoLink.php";s:4:"783f";s:23:"Classes/TinyUrl/Api.php";s:4:"402a";s:36:"Classes/TinyUrl/TinyUrlGenerator.php";s:4:"e7a0";s:29:"Classes/Utils/ConfigUtils.php";s:4:"d7f9";s:26:"Classes/Utils/UrlUtils.php";s:4:"ab2f";s:21:"Configuration/Tca.php";s:4:"0868";s:14:"doc/manual.sxw";s:4:"f62f";}',
 	'suggests' => array(
 	),
 );
