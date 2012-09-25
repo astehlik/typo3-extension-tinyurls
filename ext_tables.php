a8341eb9f3784d8828e33e3fa4527e641be8c40e
Alexander Stehlik <alexander.stehlik@googlemail.com>
2012-09-25 12:48:13 +0200

    Added icon for tinyurl records

--- ext_tables.php
+++ ext_tables.php
@@ -12,6 +12,7 @@ $TCA['tx_tinyurls_urls'] = array(
 		'enablecolumns' => array(
 			'endtime' => 'valid_until',
 		),
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif',
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/Tca.php',
 		'searchFields' => 'urlkey,target_url,target_url_hash',
 		'rootLevel' => 1,
