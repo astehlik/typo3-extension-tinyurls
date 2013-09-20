c69060490069832e837c5ebe2030bb0fbf025619
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-09-20 14:14:17 +0200

    [TASK] Updated TypoLink hook configuration
    In newer TYPO3 versions the autoloader is case sensitive, therefore
    the case of the TypoLink hook class configuration was updated.

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,7 @@ if (!defined('TYPO3_MODE')) die ('Access denied.');
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tx_tinyurls'] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/EidProcessor.php';
 
 	// register the hook for converting typolinks to tinyurls
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc']['tx_tinyurls'] = 'tx_tinyurls_hooks_typolink->convertTypolinkToTinyUrl';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc']['tx_tinyurls'] = 'Tx_Tinyurls_Hooks_TypoLink->convertTypolinkToTinyUrl';
 
 	// register the TYPO3 Backend hook class
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'tx_tinyurls_hooks_tce';
