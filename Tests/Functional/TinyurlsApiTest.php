e956dd907b6219d94298b73791fccf0878f4e00f
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-07-18 10:09:52 +0200

    [TASK] Add functional tests to Travis configuration

---
+++ Tests/Functional/TinyurlsApiTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Tx\Tinyurls\Tests\Functional;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 extension "tinyurls".                 *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\CMS\Core\Tests\FunctionalTestCase;
+
+/**
+ * Functional tests for the tinyurls API.
+ */
+class TinyurlsApiTest extends FunctionalTestCase {
+
+	/**
+	 * @var array
+	 */
+	protected $testExtensionsToLoad = array(
+		'typo3conf/ext/tinyurls',
+	);
+
+	/**
+	 * @test
+	 */
+	public function dummyTest() {
+
+	}
+}
