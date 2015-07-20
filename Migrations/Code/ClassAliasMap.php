59fa63774b018cf23c73686e0c49e9a947d7a7d7
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-07-20 12:27:30 +0200

    [BUGFIX] Add legacy classes

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'tx_tinyurls_hooks_eidprocessor' => 'Tx\\Tinyurls\\Hooks\\EidProcessor',
+	'tx_tinyurls_hooks_tce' => 'Tx\\Tinyurls\\Hooks\\TceDataMap',
+	'tx_tinyurls_hooks_typolink' => 'Tx\\Tinyurls\\Hooks\\TypoLink',
+	'tx_tinyurls_tinyurl_api' => 'Tx\\Tinyurls\\TinyUrl\\Api',
+	'tx_tinyurls_tinyurl_tinyurlgenerator' => 'Tx\\Tinyurls\\TinyUrl\\TinyUrlGenerator',
+	'tx_tinyurls_utils_configutils' => 'Tx\\Tinyurls\\Utils\\ConfigUtils',
+	'tx_tinyurls_utils_urlutils' => 'Tx\\Tinyurls\\Utils\\UrlUtils',
+);
---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,37 @@
+<?php
+die('Access denied');
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_Hooks_EidProcessor extends \Tx\Tinyurls\Hooks\EidProcessor {}
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_Hooks_TceDataMap extends \Tx\Tinyurls\Hooks\TceDataMap {}
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_Hooks_TypoLink extends \Tx\Tinyurls\Hooks\TypoLink {}
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_TinyUrl_Api extends \Tx\Tinyurls\TinyUrl\Api {}
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_TinyUrl_TinyUrlGenerator extends \Tx\Tinyurls\TinyUrl\TinyUrlGenerator {}
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_Utils_ConfigUtils extends \Tx\Tinyurls\Utils\ConfigUtils {}
+
+/**
+ * @deprecated since 1.0.0 will be removed in 3.0.0
+ */
+class Tx_Tinyurls_Utils_UrlUtils extends \Tx\Tinyurls\Utils\UrlUtils {}
