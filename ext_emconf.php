6a79100ed9d08d6dae94bd8be10f5018b5ca2d0f
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-02-15 22:36:39 +0100

    [BUGFIX] Fix TYPO3 version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Alexander Stehlik',
 	'author_email' => 'alexander.stehlik.deleteme@gmail.com',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-0.0.0',
-			'typo3' => '6.2.3-7.4.99',
+			'typo3' => '6.2.3-7.6.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
