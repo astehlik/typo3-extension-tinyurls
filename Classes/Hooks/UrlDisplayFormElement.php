950f61db6bbefbc1ba4e396770772de93303bceb
Claudia Aquino <caquino@intera.de>
2016-04-11 14:44:04 +0200

    [FEATURE] event JS on icon - copy to clipboard

--- Classes/Hooks/UrlDisplayFormElement.php
+++ Classes/Hooks/UrlDisplayFormElement.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tx\Tinyurls\Hooks;
 use Tx\Tinyurls\TinyUrl\TinyUrlGenerator;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 
 class UrlDisplayFormElement extends \TYPO3\CMS\Backend\Form\AbstractNode implements \TYPO3\CMS\Backend\Form\NodeInterface
@@ -14,79 +15,11 @@ class UrlDisplayFormElement extends \TYPO3\CMS\Backend\Form\AbstractNode impleme
     {
         $result = $this->initializeResultArray();
         $tinyUrl = new TinyUrlGenerator();
-        $result['html'] = "";
-        $result['html'] .= "<script type='text/javascript'>
-                        var fieldtoclipboard = {
-                            tooltipobj: null,
-                            hidetooltiptimer: null,
-
-                            createtooltip:function(){
-                                var tooltip = document.createElement('div')
-                                tooltip.style.cssText =
-                                    'position:absolute; background:black; color:white; padding:4px;z-index:10000;'
-                                    + 'border-radius:3px; font-size:12px;box-shadow:3px 3px 3px rgba(0,0,0,.4);'
-                                    + 'opacity:0;transition:opacity 0.3s'
-                                tooltip.innerHTML = 'Copied!'
-                                this.tooltipobj = tooltip
-                                document.body.appendChild(tooltip)
-                            },
-
-                            showtooltip:function(e){
-                                var evt = e || event
-                                clearTimeout(this.hidetooltiptimer)
-                                this.tooltipobj.style.left = evt.pageX - 10 + 'px'
-                                this.tooltipobj.style.top = evt.pageY + 15 + 'px'
-                                this.tooltipobj.style.opacity = 1
-                                this.hidetooltiptimer = setTimeout(function(){
-                                    fieldtoclipboard.tooltipobj.style.opacity = 0
-                                }, 700) // time in milliseconds before tooltip disappears
-                            },
-
-                            selectelement:function(el){
-                            var range = document.createRange() // create new range object
-                            range.selectNodeContents(el)
-                            var selection = window.getSelection() // get Selection object from currently user selected text
-                            selection.removeAllRanges() // unselect any user selected text (if any)
-                            selection.addRange(range) // add range to Selection object to select it
-                            },
-
-                            copyfield:function(e, fieldref, callback){
-                                var field = (typeof fieldref == 'string')? document.getElementById(fieldref) : fieldref
-                                callbackref = callback || function(){}
-                                if (/(textarea)|(input)/i.test(field) && field.setSelectionRange){
-                                    field.focus()
-                                    field.setSelectionRange(0, field.value.length) // for iOS sake
-                                }
-                                else if (field && document.createRange){
-                                    this.selectelement(field)
-                                }
-                                else if (field == null){ // copy currently selected text on document
-                                    field = {value:null}
-                                }
-                                var copysuccess // var to check whether execCommand successfully executed
-                                try{
-                                    copysuccess = document.execCommand('copy')
-                                }catch(e){
-                                    copysuccess = false
-                                }
-                                if (copysuccess){ // execute desired code whenever text has been successfully copied
-                                    if (e){
-                                        this.showtooltip(e)
-                                    }
-                                    callbackref(field.value || window.getSelection().toString())
-                                }
-                                return false
-                            },
-
-
-                            init:function(){
-                                this.createtooltip()
-                            }
-                        }
-
-                        fieldtoclipboard.init();</script>";
-        $result['html'] .= "<span onclick=\"return fieldtoclipboard.copyfield(event,'urldisplay')\"><img src='https://cdn0.iconfinder.com/data/icons/fatcow/32x32/clipboard_sign_out.png'/></span> ";
+        $path = ExtensionManagementUtility::extRelPath('tinyurls').'Resources/Public';
+        $result['requireJsModules'][] = $path . '/JavaScript/copytoclipboard.js';
+        $result['html'] .= "<span onclick=\"return fieldtoclipboard.copyfield(event,'urldisplay')\"><img src='$path/Icons/copytoclipboard.png'/></span>";
         $result['html'] .= "<span id='urldisplay'>".$tinyUrl->buildTinyUrl($this->data['databaseRow']['urlkey'])."</span>";
         return $result;
+
     }
 }
