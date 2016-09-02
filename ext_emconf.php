88b750a026e54611d5694193168f1f4f4ed9b940
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-09-02 18:04:58 +0200

    [TASK] Raise TYPO3 version requirement in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-0.0.0',
-			'typo3' => '6.2.3-7.6.99',
+			'typo3' => '6.2.3-8.99.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
