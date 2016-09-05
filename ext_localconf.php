6f4beeadb7eea57b8845b3b308547d281693765f
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-09-06 00:31:43 +0200

    [FEATURE] Improve rendering of urldisplay field in list module
    Resolves: #8

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,6 +16,10 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typo
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
     Tx\Tinyurls\Hooks\TceDataMap::class;
 
+// Modify the rendering of the list module.
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list.inc']['makeQueryArray']['tx_tinyurls'] =
+    \Tx\Tinyurls\Hooks\DatabaseRecordList::class;
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1460107841] = [
     'nodeName' => 'tx_tinyurls_copyable_field',
     'priority' => 40,
