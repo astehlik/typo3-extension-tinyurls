28b7590cc6514a841a2810f93271e809a9b34c5f
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-06-21 20:44:45 +0200

    [BUGFIX] Fix lower TYPO3 core version requirement in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-0.0.0',
-            'typo3' => '7.6.99-9.4.99',
+            'typo3' => '7.6.0-9.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
