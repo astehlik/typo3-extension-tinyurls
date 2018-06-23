aa8e2a45d0be83c4b23cb4cdbd25092d7856d088
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-06-23 14:05:43 +0200

    [TASK] Refactor TYPO3 ter upload handling
    Exit early if something is wrong or missing.
    
    Install a standalone version of namelesscoder/typo3-repository-client.
    
    Set version name and stability before upload.

--- .travis/deploy_ter.sh
+++ .travis/deploy_ter.sh
@@ -5,17 +5,36 @@ set -ev
 phpenv config-rm xdebug.ini
 
 # Rename our working directory, required for Extension upload to TER.
-cd .. && mv typo3-extension-tinyurls tinyurls && cd tinyurls
+cd .. && mv typo3-extension-tinyurls tinyurls
 
-if [ -n "$TRAVIS_TAG" ] && [ -n "$TYPO3_ORG_USERNAME" ] && [ -n "$TYPO3_ORG_PASSWORD" ]; then
+if [ -z "$TRAVIS_TAG" ]; then
+    echo "No Travis tag is available. Upload only runs for new tags."
+    exit 0
+fi
 
-    echo -e "Preparing upload of release ${TRAVIS_TAG} to TER\n"
+if [ -z "$TYPO3_ORG_USERNAME" ]; then
+    echo "The $TYPO3_ORG_USERNAME env var is not set."
+    exit 1
+fi
 
-    git reset --hard HEAD && git clean -fx
+if [ -z "$TYPO3_ORG_PASSWORD" ]; then
+    echo "The $TYPO3_ORG_PASSWORD env var is not set."
+    exit 1
+fi
 
-    TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
+TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
 
-    echo "Uploading release ${TRAVIS_TAG} to TER"
+if [ -z "$TAG_MESSAGE" ]; then
+    echo "The tag message could not be detected or was empty."
+    exit 1
+fi
 
-    .Build/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
-fi;
+echo "Installing TYPO3 repository client..."
+
+composer create-project --no-dev namelesscoder/typo3-repository-client typo3-repository-client
+
+echo "Uploading release ${TRAVIS_TAG} to TER"
+
+./typo3-repository-client/bin/setversion ${TRAVIS_TAG} stable tinyurls
+
+./typo3-repository-client/bin/upload tinyurls "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
