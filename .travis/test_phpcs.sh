25719cbfb9afc552e695de3df756c7772012c164
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-06-23 14:20:29 +0200

    [TASK] Do not require TYPO3 code for running code style checks

--- .travis/test_phpcs.sh
+++ .travis/test_phpcs.sh
@@ -6,8 +6,14 @@ echo "Running phpcs"
 
 phpenv config-rm xdebug.ini
 
-composer require $TYPO3_VERSION
+composer create-project --no-dev squizlabs/php_codesniffer:^3.3 codesniffer
 
-.Build/bin/phpcs --config-set installed_paths $PWD/.Build/vendor/de-swebhosting/php-codestyle/PhpCodeSniffer,Tests/CodeSniffer
+cd codesniffer
 
-.Build/bin/phpcs --standard=PSRTinyurls Classes Configuration/TCA Tests/Unit Tests/Functional ext_emconf.php ext_localconf.php
+composer require --update-no-dev de-swebhosting/php-codestyle:dev-master
+
+cd ..
+
+./codesniffer/bin/phpcs --config-set installed_paths $PWD/.Build/vendor/de-swebhosting/php-codestyle/PhpCodeSniffer,Tests/CodeSniffer
+
+./codesniffer/bin/phpcs --standard=PSRTinyurls Classes Configuration/TCA Tests/Unit Tests/Functional ext_emconf.php ext_localconf.php
