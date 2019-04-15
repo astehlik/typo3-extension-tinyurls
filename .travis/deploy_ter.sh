57c1c76923d52a23d8b4379878cc8799a126817c
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-04-15 22:39:16 +0200

    [BUGFIX] Fix TER upload script

--- .travis/deploy_ter.sh
+++ .travis/deploy_ter.sh
@@ -4,37 +4,44 @@ set -ev
 
 phpenv config-rm xdebug.ini
 
-if [ -z "$TRAVIS_TAG" ]; then
+echo "Cleanup Git repository..."
+git reset --hard HEAD && git clean -fx
+
+if [[ -z "$TRAVIS_TAG" ]]; then
     echo "No Travis tag is available. Upload only runs for new tags."
     exit 0
 fi
 
-if [ -z "$TYPO3_ORG_USERNAME" ]; then
+if [[ -z "$TYPO3_ORG_USERNAME" ]]; then
     echo "The $TYPO3_ORG_USERNAME env var is not set."
     exit 1
 fi
 
-if [ -z "$TYPO3_ORG_PASSWORD" ]; then
+if [[ -z "$TYPO3_ORG_PASSWORD" ]]; then
     echo "The $TYPO3_ORG_PASSWORD env var is not set."
     exit 1
 fi
 
-TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
+TAG_MESSAGE=`git tag -n10 -l ${TRAVIS_TAG} | sed 's/^[0-9.]*[ ]*//g'`
 
-if [ -z "$TAG_MESSAGE" ]; then
+if [[ -z "$TAG_MESSAGE" ]]; then
     echo "The tag message could not be detected or was empty."
     exit 1
 fi
 
-# Rename our working directory, required for Extension upload to TER.
-cd .. && mv typo3-extension-tinyurls tinyurls
+echo "Extracted tag message: $TAG_MESSAGE"
 
 echo "Installing TYPO3 repository client..."
 
 composer create-project --no-dev namelesscoder/typo3-repository-client typo3-repository-client
 
+echo "Renaming repository folder to match extension key..."
+cd ..
+mv typo3-extension-tinyurls tinyurls
+cd tinyurls
+
+echo "Setting version to ${TRAVIS_TAG#"v"}"
+../typo3-repository-client/bin/setversion ${TRAVIS_TAG#"v"}
+
 echo "Uploading release ${TRAVIS_TAG} to TER"
-
-./typo3-repository-client/bin/setversion ${TRAVIS_TAG} stable tinyurls
-
-./typo3-repository-client/bin/upload tinyurls "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
+../typo3-repository-client/bin/upload "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
