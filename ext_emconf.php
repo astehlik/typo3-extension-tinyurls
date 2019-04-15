32aeb9e91486e332617a68d68b523cb7432fc43e
Alexander Stehlik <astehlik@users.noreply.github.com>
2019-04-15 20:58:39 +0200

    [TASK] Allow version 9.5 in ext_emconf.php
    Resolves: #20

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-0.0.0',
-            'typo3' => '7.6.0-9.4.99',
+            'typo3' => '7.6.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
