46d8bcd571e429d1efa408969e42aeb99ef68dcf
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-06-21 20:46:26 +0200

    [BUGFIX] Do not upload code coverate for pull requests

--- .travis/test_phpunit.sh
+++ .travis/test_phpunit.sh
@@ -11,14 +11,14 @@ else
 fi
 
 
-if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" ]]; then
+if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" && "${CODECLIMATE_REPO_TOKEN}" != "" ]]; then
     .Build/bin/phpunit --coverage-clover .Build/Logs/clover.xml --whitelist Classes Tests/Unit/
 else
     phpenv config-rm xdebug.ini
     .Build/bin/phpunit Tests/Unit/
 fi
 
-if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" ]]; then
+if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" && "${CODECLIMATE_REPO_TOKEN}" != "" ]]; then
     composer require --dev codeclimate/php-test-reporter:dev-master;
     .Build/bin/test-reporter --coverage-report .Build/Logs/clover.xml;
 fi
