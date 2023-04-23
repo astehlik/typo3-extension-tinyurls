ec0bcf9fbe24ef7f10e4bab4d73568e45bc8cef6
Alexander Stehlik <alex@stehlik-online.de>
2023-04-23 17:39:41 +0200

    [TASK] Add script for cleaning up files for TER upload

---
+++ Build/cleanup_for_ter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f ready_for_release.txt ]; then
+  echo "The file ready_for_release.txt does not exists. Make sure you run this script in the right directory!"
+  exit 1
+fi
+
+if [ -d ".git" ]; then
+  git clean -fdX
+fi
+
+rm -Rf .git
+rm -Rf .github
+rm -Rf Build
+rm -f .gitignore
+rm ready_for_release.txt
