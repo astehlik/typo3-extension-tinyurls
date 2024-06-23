bf5cc272c7dff0e49b1170ee8da955bc53363dc9
Alexander Stehlik <alex@stehlik-online.de>
2024-06-23 20:39:43 +0200

    [TASK] Improve TER cleanup

--- Build/cleanup_for_ter.sh
+++ Build/cleanup_for_ter.sh
@@ -13,8 +13,13 @@ fi
 
 rm -Rf .git
 rm -Rf .github
+rm -Rf .phpstorm.meta.php
+rm -Rf .Build
 rm -Rf Build
-rm -f .gitignore
 rm -Rf Tests
 rm -Rf typo3temp
+rm -f .codeclimate.yml
+rm -f .crowdin.yml
+rm -f .editorconfig
+rm -f .gitignore
 rm ready_for_release.txt
