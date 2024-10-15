5a38d6163094c46e2b3c406faf41f1988568d27b
Alexander Stehlik <alex@stehlik-online.de>
2024-10-15 23:10:11 +0200

    [TASK] Update dependencies in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.3.99',
-            'typo3' => '13.2.0-13.2.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
