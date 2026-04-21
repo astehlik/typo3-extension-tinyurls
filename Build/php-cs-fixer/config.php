ae644db0733c1c8ad111835887ba7e50650127ad
Alexander Stehlik <alex@stehlik.it>
2026-04-21 22:49:17 +0200

    chore: improve excluded paths of PHP CS Fixer

--- Build/php-cs-fixer/config.php
+++ Build/php-cs-fixer/config.php
@@ -12,11 +12,12 @@ if (PHP_SAPI !== 'cli') {
 $rules = include __DIR__ . '/../../.Build/vendor/de-swebhosting/php-codestyle/PhpCsFixer/PerCsDefaultRules.php';
 
 return (new Config())
+    ->setCacheFile(__DIR__ . '/../../.cache/php-cs-fixer.cache')
     ->setFinder(
         (new Finder())
             ->ignoreVCSIgnored(true)
             ->in(realpath(__DIR__ . '/../../'))
-            ->exclude(['.Build'])
+            ->exclude(['.cache', '.Build', 'typo3temp', 'var'])
             ->notName(['ext_emconf.php', 'ext_localconf.php', 'ext_tables.php']),
     )
     ->setRiskyAllowed(true)
